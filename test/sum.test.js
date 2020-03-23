bad5af5abd8a1269880417abc3da7c27357ec16d
Wesley Ferreira <wsf.ley@gmail.com>
2020-03-23 17:02:11 -0300

    Adicionando mais testes

--- test/sum.test.js
+++ test/sum.test.js
@@ -3,3 +3,16 @@ const sum = require('../sum');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('adds 2 + 2 to equal 4', () => {
+  expect(sum(2, 2)).toBe(4);
+});
+
+test('adds 3 + 5 to equal 8', () => {
+  expect(sum(3, 5)).toBe(8);
+});
+
+
+test('adds 3 + 6 to equal 9', () => {
+  expect(sum(3, 6)).toBe(9);
+});
